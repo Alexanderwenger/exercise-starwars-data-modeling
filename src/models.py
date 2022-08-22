2f99f60e054ffc2ddfc71194ead5ff6517d8ae88
Alexanderwenger <awengern@gmail.com>
2022-08-22 03:06:00 +0000

    tarea lista

--- src/models.py
+++ src/models.py
@@ -8,26 +8,38 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class People(Base):
+    __tablename__ = 'people'
+    NameID = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    homeworld = Column(String(250), ForeignKey('planets.Name'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Planets(Base):
+    __tablename__ = 'planets'
+    PlanetID = Column(Integer, primary_key=True)
+    Name = Column(String(250), nullable=False, unique=True)
+    Climate = Column(String(50), nullable=True)
+    Residents = Column(Integer, nullable=True)
+    rel = relationship(People)
 
-    def to_dict(self):
-        return {}
+class FavPeople(Base):
+    __tablename__ = 'favpeople'
+    FavPeopleID = Column(Integer, primary_key=True)
+    people = Column(String(250), ForeignKey('people.name'))
+    user_ID = Column(String(250), ForeignKey('users.Email'))
+
+class FavPlanets(Base):
+    __tablename__ = 'favplanet'
+    FavPlanetID = Column(Integer, primary_key=True)
+    planets = Column(String(250), ForeignKey('planets.name'))
+    user_ID = Column(String(250), ForeignKey('users.Email'))
+
+class Users(Base):
+    __tablename__ = 'users'
+    UserID = Column(Integer, primary_key=True)
+    Username = Column(String(250), nullable=False, unique=True)
+    Email = Column(String(100), nullable=False, unique=True)
+    Password = Column(String(50), nullable=False, unique=True)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
